fdfaae2ac4061f3083de743d0d3727c77f952b41
ekaterinadegtiareva <edegtiareva@evolution.com>
2025-11-13 00:48:55 +0200

    Add before each helper and one worker

--- tests/allOtherTests.test.ts
+++ tests/allOtherTests.test.ts
@@ -3,6 +3,26 @@ import { test, expect } from '@playwright/test';
 let baseURL: string = 'http://localhost:3000/users';
 
 test.describe('User management API', () => {
+    test.beforeEach(async ({ request }) => {
+        const responseAllUsers = await request.get(`${baseURL}`);
+        expect(responseAllUsers.status()).toBe(200);
+        const responseUsers = await responseAllUsers.json();
+        const numberOfObjects = responseUsers.length;
+        console.log('number of objects' + numberOfObjects);
+        let userIds: number[] = [];
+    for (let i = 0; i < numberOfObjects; i++) {
+        let userId = responseUsers[i].id;
+        userIds.push(userId);
+    }
+    for (let i = 0; i < numberOfObjects; i++) {
+        let response = await request.delete(`${baseURL}/${userIds[i]}`);
+        expect.soft(response.status()).toBe(200);
+        }
+        const response = await request.get(`${baseURL}`);
+        expect.soft(response.status()).toBe(200);
+        const responseBody = await response.text();
+            expect.soft(responseBody).toBe('[]');
+    });
 
     test('find user: should return a user by ID', async ({ request }) => {
         //create a user
@@ -47,5 +67,29 @@ test.describe('User management API', () => {
         await request.delete(`${baseURL}`+ '/' + userId);
         const getResponse = await request.get(baseURL + '/' + userId);
         expect(getResponse.status()).toBe(404);
+    });
+
+    test('get user id info', async ({ request }) => {
+        const response1 = await request.post(`${baseURL}`);
+        const response2 = await request.post(`${baseURL}`);
+        const responseAllUsers = await request.get(`${baseURL}`);
+        const responseUsers = await responseAllUsers.json();
+        const numberOfObjects = responseUsers.length;
+        console.log('number of objects', numberOfObjects);
+
+        let userIds: number[] = [];
+
+        for (let i = 0; i < numberOfObjects; i++) {
+            let UserId = responseUsers[i].id;
+            userIds.push(UserId);
+        }
+        console.log(userIds);
+        //new code
+        for (let i = 0; i < numberOfObjects; i++) {
+            // delete user by id
+            let response = await request.delete(`${baseURL}/${userIds[i]}`);
+            // validate the response status code
+            expect.soft(response.status()).toBe(200);
+        }
     });
 });
