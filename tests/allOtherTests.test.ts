36ee4d90b36226055195cc91836b63bb879f81cf
ekaterinadegtiareva <edegtiareva@evolution.com>
2025-11-13 17:48:12 +0200

    Moved helper function to a separate file

--- tests/allOtherTests.test.ts
+++ tests/allOtherTests.test.ts
@@ -1,30 +1,14 @@
 // tests/api.spec.ts
 import { test, expect } from '@playwright/test';
+import { cleanUpUsers } from "../src/cleanup";
 let baseURL: string = 'http://localhost:3000/users';
 
 test.describe('User management API', () => {
     test.beforeEach(async ({ request }) => {
-        const responseAllUsers = await request.get(`${baseURL}`);
-        expect(responseAllUsers.status()).toBe(200);
-        const responseUsers = await responseAllUsers.json();
-        const numberOfObjects = responseUsers.length;
-        console.log('number of objects' + numberOfObjects);
-        let userIds: number[] = [];
-    for (let i = 0; i < numberOfObjects; i++) {
-        let userId = responseUsers[i].id;
-        userIds.push(userId);
-    }
-    for (let i = 0; i < numberOfObjects; i++) {
-        let response = await request.delete(`${baseURL}/${userIds[i]}`);
-        expect.soft(response.status()).toBe(200);
-        }
-        const response = await request.get(`${baseURL}`);
-        expect.soft(response.status()).toBe(200);
-        const responseBody = await response.text();
-            expect.soft(responseBody).toBe('[]');
+        await cleanUpUsers(request);
     });
 
-    test('find user: should return a user by ID', async ({ request }) => {
+    test('find user: should return a user by ID', async ({request}) => {
         //create a user
         const response = await request.post(`${baseURL}`);
         const responseBody = await response.json();
@@ -34,7 +18,7 @@ test.describe('User management API', () => {
         expect(getResponse.status()).toBe(200);
     });
 
-    test('find user: should return 404 if user not found', async ({ request }) => {
+    test('find user: should return 404 if user not found', async ({request}) => {
         const response = await request.post(`${baseURL}`);
         const responseBody = await response.json();
         const userId = responseBody.id;
@@ -43,7 +27,7 @@ test.describe('User management API', () => {
         expect(getResponse.status()).toBe(404);
     });
 
-    test('create user: should add a new user', async ({ request }) => {
+    test('create user: should add a new user', async ({request}) => {
         const response = await request.post(`${baseURL}`);
         expect(response.status()).toBe(201);
         const responseBody = await response.json();
@@ -51,7 +35,7 @@ test.describe('User management API', () => {
         console.log(responseBody);
     });
 
-    test('delete user: should delete a user by ID', async ({ request }) => {
+    test('delete user: should delete a user by ID', async ({request}) => {
         const response = await request.post(`${baseURL}`);
         const responseBody = await response.json();
         const userId = responseBody.id;
@@ -60,7 +44,7 @@ test.describe('User management API', () => {
         expect(deletedUser.status()).toBe(404);
     });
 
-    test('delete user: should return 404 if user not found', async ({ request }) => {
+    test('delete user: should return 404 if user not found', async ({request}) => {
         const response = await request.post(`${baseURL}`);
         const responseBody = await response.json();
         const userId = responseBody.id;
@@ -69,7 +53,7 @@ test.describe('User management API', () => {
         expect(getResponse.status()).toBe(404);
     });
 
-    test('get user id info', async ({ request }) => {
+    test('get user id info', async ({request}) => {
         const response1 = await request.post(`${baseURL}`);
         const response2 = await request.post(`${baseURL}`);
         const responseAllUsers = await request.get(`${baseURL}`);
@@ -80,8 +64,8 @@ test.describe('User management API', () => {
         let userIds: number[] = [];
 
         for (let i = 0; i < numberOfObjects; i++) {
-            let UserId = responseUsers[i].id;
-            userIds.push(UserId);
+            let userId = responseUsers[i].id;
+            userIds.push(userId);
         }
         console.log(userIds);
         //new code
